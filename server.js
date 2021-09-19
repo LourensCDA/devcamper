f26a3c87504ba84b4525dfc4a41a3a5b8ee44c63
LourensCDA <lourens@cdasolutions.co.za>
2021-09-19 04:58:24 +0200

    confirm db connection

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const color = require('colors');
+const { sequelize } = require('./models');
 
 // route files
 const bootcamps = require('./routes/bootcamps');
@@ -20,6 +22,12 @@ if (process.env.NODE_ENV === 'development') {
 // mount routes
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+app.listen(PORT, async () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+  );
+  // confirm db connection
+  await sequelize.authenticate();
+  const db_ver = await sequelize.databaseVersion();
+  console.log(`PostgreSQL Connected: ${db_ver}`.cyan.underline.bold);
 });

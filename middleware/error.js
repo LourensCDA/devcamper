413e8a81c95360d02ed916e023b0d85193dcfbc8
LourensCDA <lourens@cdasolutions.co.za>
2021-09-26 09:16:46 +0200

    added error array of objects

--- middleware/error.js
+++ middleware/error.js
@@ -2,12 +2,12 @@ const ErrorResponse = require('../utils/errorResponse');
 
 const errorHandler = (err, req, res, next) => {
   let error = { ...err }; // ... spread operator
+  let error_fields = new Array();
 
   error.message = err.message;
 
   // log to console for dev
   console.log(err.stack.red);
-  console.log(error);
 
   // Sequelize bad Object ID
   if (err.name === 'SequelizeDatabaseError') {
@@ -15,16 +15,35 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404);
   }
 
-  // Sequelize duplicate ket
+  // Sequelize duplicate key
   if (err.name === 'SequelizeUniqueConstraintError') {
     const message = `Duplicate field value entered`;
     error = new ErrorResponse(message, 400);
   }
 
-  res.status(error.statusCode || 500).json({
-    success: false,
-    message: error.message || 'Server Error',
-  });
+  // Sequelize validaiton error
+  if (err.name === 'SequelizeValidationError') {
+    const message = `Invalid data provided`;
+    error = new ErrorResponse(message, 400);
+    // display fields as array of objects
+    error_fields = err.errors.map((element) =>
+      Object.assign({}, { field: element.path, description: element.message })
+    );
+  }
+
+  // return arrary of errors per field if available
+  if (error_fields.length > 0) {
+    res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message || 'Server Error',
+      errors: error_fields,
+    });
+  } else {
+    res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message || 'Server Error',
+    });
+  }
 };
 
 module.exports = errorHandler;

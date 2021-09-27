78e3d111613f27f85eb153e5487d52e463ec36a6
LourensCDA <lourens@cdasolutions.co.za>
2021-09-27 20:31:05 +0200

    added options for individualHooks

--- models/bootcamp.js
+++ models/bootcamp.js
@@ -167,7 +167,6 @@ module.exports = (sequelize, DataTypes) => {
           );
         },
       },
-      individualHooks: true,
       sequelize,
       tableName: 'bootcamps',
       modelName: 'Bootcamp',
